5eb366fc83c88fe86818151f68ae257c6b3b9edf
Parsa <psaeedi1381@gmail.com>
2022-08-14 19:19:10 -0700

    Link Changes to Relink After Sign In

--- src/SignIn.js
+++ src/SignIn.js
@@ -4,11 +4,14 @@ export default function SignIn(props) {
 
     let center = props.signedIn ? <img className="profilePicture" src={props.profilePicture}/> : <span className='guide'>Link your Spotify profile to select its playlists</span>
 
+    let linkText = props.signedIn ? "RELINK" : "LINK"
+
+
     return(
         <div className="signIn">
             <div className="name"><input type="text" className="name1" id="name1" spellCheck="false" value={props.name} onChange={props.handleChangeName} placeholder={props.placeholder}/></div>
             {center}
-            <button onClick={props.handleLinkButton}>LINK</button>
+            <button onClick={props.handleLinkButton}>{linkText}</button>
         </div>
     )
 }
